03cb47ed51f999034a587a69dff1d91cc0fa82ea
Russel Pugh <russ_scooby@yahoo.com>
2020-02-27 03:40:12 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -7,7 +7,7 @@ def subtraction(num1, num2)
 end
 
 def division(num1, num2)
-  num1 / num2
+  num2 / num1
 end
 
 def multiplication(num1, num2)
@@ -15,9 +15,12 @@ def multiplication(num1, num2)
 end
 
 def modulo(num1, num2)
-
+  num2 / num1
 end
 
 def square_root(num)
-
+  Math.sqrt(num)
 end
+
+addition(5,4)
+subtraction(10,5)
